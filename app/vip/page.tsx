f291f89a2adfc6302b610574e6784dbdd9ab1670
amir <xdemigodxa@gmail.com>
2026-06-13 01:52:53 +0330

    شششش

---
+++ app/vip/page.tsx
@@ -0,0 +1,8 @@
+export default function VipPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <p>ok</p>
+      <p>سلام خوبی</p>
+    </div>
+  );
+}
